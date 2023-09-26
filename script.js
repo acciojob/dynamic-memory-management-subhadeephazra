04337bfdfd7af14249a332bdc9331b10788bdf82
Subhadeep Hazra <124555668+subhadeephazra@users.noreply.github.com>
2023-09-26 16:07:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,43 @@
-// Complete the js code
+document.addEventListener("DOMContentLoaded", function () {
+    const generateButton = document.getElementById("generateButton");
+    const removeButton = document.getElementById("removeButton");
+    const elementContainer = document.getElementById("elementContainer");
+    const memoryUsage = document.getElementById("memoryUsage");
 
-const limit = 50; // Heap memory usage limit in MB
-let elements = []; // Array to hold generated DOM elements
+    let elementCount = 0;
+    let heapMemoryUsage = 0;
+    const memoryLimit = 50 * 1024 * 1024; // 50 MB in bytes
 
-// Function to generate DOM elements
-const generateElements = () => {
-  // complete the function
-  updateMemoryUsage();
-};
+    // Function to generate elements
+    generateButton.addEventListener("click", function () {
+        for (let i = 0; i < 10000; i++) {
+            const element = document.createElement("div");
+            element.innerText = "Element " + (++elementCount);
+            elementContainer.appendChild(element);
 
-// Function to remove DOM elements
-const removeElements = () => {
-  // complete the function
-  updateMemoryUsage();
-};
+            // Check heap memory usage
+            if (performance.memory.usedJSHeapSize >= memoryLimit) {
+                alert("Memory usage has exceeded 50 MB. Please optimize your actions to reduce memory consumption.");
+                break;
+            }
+        }
 
-// Function to update memory usage display
-const updateMemoryUsage = () => {
-  // Complete this function
-};
+        // Update memory usage display
+        updateMemoryUsage();
+    });
 
-// Add event listeners to buttons
-document.getElementById("generate").addEventListener("click", generateElements);
-document.getElementById("remove").addEventListener("click", removeElements);
+    // Function to remove elements
+    removeButton.addEventListener("click", function () {
+        elementContainer.innerHTML = "";
+        elementCount = 0;
 
-// Set interval to update memory usage every second
-setInterval(updateMemoryUsage, 1000);
+        // Update memory usage display
+        updateMemoryUsage();
+    });
+
+    // Function to update memory usage display
+    function updateMemoryUsage() {
+        heapMemoryUsage = performance.memory.usedJSHeapSize;
+        memoryUsage.textContent = "Heap Memory Usage: " + (heapMemoryUsage / (1024 * 1024)).toFixed(2) + " MB";
+    }
+});
